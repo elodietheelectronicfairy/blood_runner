36cb430662ce803f73d9f80c892991811f28ea99
Elodie Tscharke <datby88@gnail.com>
2017-10-24 23:02:56 +0000

    commit seccond edit

--- blood_runner.py
+++ blood_runner.py
@@ -5,9 +5,9 @@ import os
 def clear():
     os.system('cls' if os.name=='nt' else 'clear')
 
-clear()
-print ("Welcome to:")
-print ("""
+def main_menu
+    print ("Welcome to:")
+    print ("""
 ______ _                 _  ______
 | ___ \ |               | | | ___ \
 | |_/ / | ___   ___   __| | | |_/ /   _ _ __  _ __   ___ _ __
@@ -16,4 +16,26 @@ ______ _                 _  ______
 \____/|_|\___/ \___/ \__,_| \_| \_\__,_|_| |_|_| |_|\___|_|
                         ______
                        |______|
-""")
+    """)
+
+    print ("What would you like to do?")
+    print ("""
+    1. Start game
+    2. Get help
+    0. Exit
+    """)
+
+    main = input(":$ ")
+
+    if main in ('1');
+        Ok starting game!
+        game()
+    elif main in ('2');
+        help()
+    elif menu in ('0')
+        exit()
+    else;
+        clear()
+        print ("wat?")
+        main_menu()
+    
